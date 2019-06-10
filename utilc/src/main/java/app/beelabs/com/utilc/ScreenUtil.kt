d4b820f82065c891b7a6ad780c77320122306b22
ary <ary@clappingape.com>
2019-06-10 10:53:42 +0700

    update for screenutil function

--- utilc/src/main/java/app/beelabs/com/utilc/ScreenUtil.kt
+++ utilc/src/main/java/app/beelabs/com/utilc/ScreenUtil.kt
@@ -1,21 +1,18 @@
 package app.beelabs.com.utilc
 
-import android.app.Activity
 import android.util.DisplayMetrics
 
 class ScreenUtil {
 
     companion object {
 
-        fun getScreenWidth(x: Activity): Int {
+        fun getScreenWidth(): Int {
             val metrics = DisplayMetrics()
-            x.windowManager.defaultDisplay.getMetrics(metrics)
             return metrics.widthPixels
         }
 
-        fun getScreenHeight(x: Activity): Int {
+        fun getScreenHeight(): Int {
             val metrics = DisplayMetrics()
-            x.windowManager.defaultDisplay.getMetrics(metrics)
             return metrics.heightPixels
         }
     }
---
+++ utilc/src/test/java/app/beelabs/com/utilc/ScreenUtilTest.kt
@@ -0,0 +1,25 @@
+//package app.beelabs.com.utilc
+//
+//import android.app.Activity
+//import com.nhaarman.mockitokotlin2.mock
+//import org.junit.Before
+//import org.junit.Test
+//import org.mockito.Mock
+//import org.mockito.MockitoAnnotations
+//
+//internal class ScreenUtilTest {
+//
+//    @Mock
+//    private lateinit var mActivity: Activity
+//
+//    @Before
+//    fun init() {
+//        mActivity = Activity()
+//    }
+//
+//    @Test
+//    fun `screen width`() {
+//        mActivity = Activity()
+//        ScreenUtil.getScreenWidth(mActivity)
+//    }
+//}
